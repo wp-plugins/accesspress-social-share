8bdd7738a526cc42f2e9101d0c3b9d59b850ac4e
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-03 11:45:44 +0000

    * Added the shortcode for share count number display in the content.
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-social-share/tags/2.0.7@1211764 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/backend/how-to-use.php
@@ -0,0 +1,43 @@
+<?php defined('ABSPATH') or die("No script kiddies please!"); ?>
+<p>Basically there are four main settings tabs that will help you to setup the plugin to work properly.</p>
+<dl>
+  <dt><strong>Social Networks</strong></dt>
+  <dd><p>In this tab you can choose which social media icons you want to display in the frontend. Here you can order the apperance of the social icons simply by drag and drop of each social icons.</p></dd>
+    
+  <dt><strong>Share options</strong></dt>
+  <dd><p>In this tab you can select the options where you want to display social media share icons.</p></dd>
+  
+  <dt><strong>Display settings</strong></dt>
+  <dd><p>In this tab you can customize the apperance of the social media share icons. You can choose the options to display the share icons below the contennt, above the content or you can choose an options to display in both below and above content. Also you can choose the theme from the pre available 5 themes.</p></dd>
+  
+  <dt><strong>Miscellaneous settings</strong></dt>
+  <dd><p>In this tab you can do the additional settings for the plugin.
+    <ul class="how-list">
+      <li><i class="fa fa-check"></i>You can setup the twitter username.</li>
+      <li><i class="fa fa-check"></i>You can enable/disable the social counter.</li>
+      <li><i class="fa fa-check"></i>Share link - You can enable the share options either in new tab/window or in same widow.</li>
+      <li><i class="fa fa-check"></i>Cache settings - Here you can set the cache settings of the social share counter in hour format.</li>
+      <li><i class="fa fa-check"></i>Email settings - If you have enabled the email settings you can setup the email header and body part.</li>
+    </ul>
+  </p></dd>
+
+  <dt><strong>Shortcode</strong></dt>
+  <dd><p>You can use shortcode for the display of the social share in the contents. Optionally You can enter the name of the networks you want to display. The networks will be displayed in the order of entered networks.
+    <ul class="how-list">
+      <li><i class="fa fa-check"></i>Example 1: <code>[apss-share]</code></li>
+      <li><i class="fa fa-check"></i>Example 2: <code>[apss-share networks='facebook, twitter, pinterest']</code></li>
+      <li><i class="fa fa-check"></i>Available network parameters are: facebook, twitter, google-plus, pinterest, linkedin, digg, email, print</li>
+      </ul>
+  </p></dd>
+  <dd>
+    <p>You can use shortcode [apss-count] for the display of the social share count only in the contents. You need to enter network name you want to display.
+        <ul class="how-list">
+          <li><i class="fa fa-check"></i>Example 1: <code>[apss-count network='facebook']</code></li>
+          <li><i class="fa fa-check"></i>Available network parameters are: facebook, twitter, google-plus, pinterest, linkedin</li>
+        </ul>
+    </p>
+  </dd>
+</dl>
+
+
+
